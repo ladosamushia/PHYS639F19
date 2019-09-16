169ce6f51fda77e79649db476279e56912a2f3d1
alexandercoon <42680458+alexandercoon@users.noreply.github.com>
2019-09-16 09:40:40 -0500

    Add files via upload

--- NicoleCoon/euler method.py
+++ NicoleCoon/euler method.py
@@ -6,38 +6,49 @@ Created on Tue Sep 10 13:27:39 2019
 """
 import numpy as np
 import matplotlib.pyplot as plt
-def fprime(x):
-    return np.sin(x)
-# create given point, and upper and lower bounds
-fstart = 0
-f0 = -1
-xi = -2*np.pi
-xf = 2*np.pi
-dx = .0001
-#define 2 arrays: store x and y values
-x = np.array([fstart])
-y = np.array([f0])
-i = fstart-dx
-while i >xi:
-    xval = np.array([i]) #incrment x
-    x = np.concatenate((x,xval)) #add to arrat
-    dfdx = ((1+np.cos(i))/(1+y[-1]+y[-1]))
-    yval = y[-1]-dfdx*dx #incrment y
-    yval = np.array([yval])
-    y = np.concatenate((y,yval))
-    i = i-dx
-i = fstart+dx
-#flip to put lowest values first
-x=np.flip(x)
-y=np.flip(y)
-while i<xf:
-    xval = np.array([i])
-    x = np.concatenate((x,xval))
-    dfdx = (1+np.cos(i))/(1+2*y[-1])
-    yval = y[-1]+dfdx*dx
-    yval = np.array([yval])
-    y = np.concatenate((y,yval))
-    i = i+dx
+# create basic funcitons
+def flinear(x, fx):
+    return x
 
-plt.plot(x,y)
-    
+def fcos(x, fx):
+    return np.cos(x)
+
+def fnegexp(x, fx):
+    return np.exp(-x)
+
+def complicated_function(x, fx):
+    return (1 + np.cos(x))/(1 + 2*fx)
+#deriv takes 2 inputs, x and function of x
+def euler(x0,f0,xi,xf,dx,deriv):    
+    #define 2 arrays: store x and y values
+    x = np.array([x0])
+    y = np.array([f0])
+    i = x0-dx
+    while i >xi:
+        xval = np.array([i]) #incrment x
+        x = np.concatenate((x,xval)) #add to arrat
+        dfdx = deriv(x[-1],y[-1]) #derv evaluated
+        yval = y[-1]-dfdx*dx #incrment y
+        yval = np.array([yval])
+        y = np.concatenate((y,yval))
+        i = i-dx
+    i = x0+dx
+    #flip to put lowest values first
+    x=np.flip(x)
+    y=np.flip(y)
+    #repeat but forward
+    while i<xf:
+        xval = np.array([i])
+        x = np.concatenate((x,xval))
+        dfdx = deriv(x[-1],y[-1])
+        yval = y[-1]+dfdx*dx
+        yval = np.array([yval])
+        y = np.concatenate((y,yval))
+        i = i+dx
+    return(x,y)
+# break down region into peices, and plot along piece of region
+NIC = 10
+IC = np.linspace(-2, 2, NIC)
+for i in range(NIC):
+    x,y = euler(0,IC,0,10,.001,complicated_function)    
+    plt.plot(x, y)

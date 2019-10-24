e1171ffb7c5592626d21ba4f2e8544f311cd712c
NathanM1823 <53013372+NathanM1823@users.noreply.github.com>
2019-10-24 12:54:46 -0500

    Update Spins With Energy Probability.py

--- NathanMarshall/Random_Processes3/Spins With Energy Probability.py
+++ NathanMarshall/Random_Processes3/Spins With Energy Probability.py
@@ -9,8 +9,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation as animate
 
-numx = 5
-numy = 5
+numx = 10
+numy = 10
 x = np.arange(0, numx)
 y = np.arange(0, numy)
 xx, yy = np.meshgrid(x, y)
@@ -18,7 +18,8 @@ T = 0.1
 spins = np.random.choice([-1, 1], (numx, numy))
 
 fig, ax = plt.subplots(1,1)
-Q = ax.quiver(xx, yy, np.zeros((numx, numy)), spins, scale=25)
+Q = ax.quiver(xx, yy, np.zeros((numx, numy)), spins, spins, scale=25, 
+              cmap='plasma')
 ax.scatter(xx, yy)
 ax.set_aspect('equal')
 #%%
@@ -47,7 +48,7 @@ def update(frame):
         p_up = np.exp(-energy / T) / (np.exp(-energy / T) + np.exp(energy / T))
         p_down = np.exp(energy / T) / (np.exp(-energy / T) + np.exp(energy / T))
     spins[randrow][randcol] = np.random.choice([-1, 1], p=[p_down, p_up])
-    Q.set_UVC(np.zeros((numx, numy)), spins)
+    Q.set_UVC(np.zeros((numx, numy)), spins, spins)
     return(Q)
     
 anim = animate(fig, update, frames=1000, interval=10, repeat=False)
